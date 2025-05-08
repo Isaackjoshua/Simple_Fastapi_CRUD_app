b2f4421f6e2956c9af3221ec4023fd4a8765b829
Isaackjoshua <isaackjoshua23@gmail.com>
2025-05-08 23:47:13 +0300

    Added Exceptions and status code

--- app/myapi.py
+++ app/myapi.py
@@ -6,10 +6,10 @@ from random import randrange
 
 app = FastAPI()
 
-class Post(BaseModel):
+class Post(BaseModel): #So this is our schema and what it does is it specifies the data format that our api recieves from the front end.
     title: str
     content: str
-    Published: bool
+    Published:Optional[bool] = True
     rating: Optional[int] = None # Optional allows us to not have to fill in the rating field if we do not want to.
     
     
@@ -22,6 +22,11 @@ def find_post(id):
         if i["id"] == id:
             return i
       
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i 
+        
 
 @app.get("/posts")
 async def root():
@@ -31,14 +36,37 @@ async def root():
 def createpost(post: Post): 
     post_dict = post.dict()# This converts the post variable to a dictionary from pydantic dictionary.
     post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)#Hapa ni kwamba tuna add the input (post created on the frot end ) to our storage variable my_posts.
+    my_posts.append(post_dict)
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_posts(id: int, response: Response):# that int keyword int the parameter helps us ensure that the id is an integer,as a path is a always a string
+def get_posts(id: int, response: Response):
     post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was no found")# raise exception
        # response.status_code = status.HTTP_404_NOT_FOUND
        #return {"Message": f"Post with id {id} was no found"}
     return{"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)#Using a 204 will restrict you from sending any more data
+def delete_post(id: int): #passing the int keyword changes the string type id into the interger form.
+    #deleting post
+    #find the index in the array that has the required ID
+    # my_posts.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'post with id {id} does not exist')
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    #print(post)
+     index = find_index_post(id)
+     if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail = f'post with id {id} does not exist')
+     post_dict = post.dict()
+     post_dict['id'] = id
+     my_posts[index] = post_dict
+     return{'data': post_dict}
